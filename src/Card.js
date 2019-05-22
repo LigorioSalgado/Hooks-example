9b251c524a7b0c51bc3b3b1d4b8285cfeb277476
ligorio93 <edwin@devf.mx>
2019-05-22 12:21:48 -0500

    chnages in card

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 
 
-function Card(){
+function Card({characater}){
 
     return(
         <div className="col-md-4 my-3">
             <div className="card" >
-            <img src="https://picsum.photos/300/200" className="card-img-top" />
+            <img src={characater.image} className="card-img-top" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{charcater.name}</h5>
+                <ul>
+                    <li>Status:{characater.status}</li>
+                    <li>Specie:{character.species}</li>
+                    <li>Gender:{character.gender}</li>
+                    <li>Origin:{characater.origin.name}</li>
+                </ul>
             </div>
             </div>
         </div>
